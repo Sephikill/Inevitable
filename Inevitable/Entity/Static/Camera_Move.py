9dec1ebfba3012afc9b887d18bfd89daa27529f5
Sephikill <81938501+Sephikill@users.noreply.github.com>
2021-08-28 12:16:52 -0400

    Delete Inevitable directory

--- Inevitable/Entity/Static/Camera_Move.py
+++
@@ -1,6 +0,0 @@
-class Camera_Move():
-	pass
-"""when player lands on it move everthing 
-eg when player reaches right side of screen and there
-more stuff to do move everthing to the left side.  
-"""
